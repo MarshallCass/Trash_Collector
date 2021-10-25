e13fa8c6c721dc231c22c4935c3bb8a848ece3b5
Marshall C <MCass2101@gmail.com>
2021-10-25 15:45:22 -0400

    new pull

--- trash_collectors/employees/views.py
+++ trash_collectors/employees/views.py
@@ -1,11 +1,10 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.apps import apps
 from django.urls import reverse
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
-from django.apps import apps
 from datetime import date
-
 from .models import Employee
 
 # Create your views here.
@@ -15,9 +14,8 @@ from .models import Employee
 
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-    Customer = apps.get_model('customer.Customer')
+    Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
-
 
 @login_required
 def create(request):
@@ -26,12 +24,12 @@ def create(request):
         name_from_form = request.POST.get('name')
         address_from_form = request.POST.get('address')
         zip_from_form = request.POST.get('zip_code')
-        new_employee = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form)
+        new_employee = Employee(name=name_from_form, user=logged_in_user,
+                                address=address_from_form, zip_code=zip_from_form)
         new_employee.save()
-        return HttpResponseRedirect(reverse('employee:index'))
+        return HttpResponseRedirect(reverse('employees:index'))
     else:
-        return render(request, 'employee/create.html')
-
+        return render(request, 'employees/create.html')
 
 @login_required
 def edit_profile(request):
@@ -40,14 +38,14 @@ def edit_profile(request):
     if request.method == "POST":
         name_from_form = request.POST.get('name')
         address_from_form = request.POST.get('address')
-        zip_from_form = request.POST.get('zip_code')
+        zip_from_form = request.POST.get('zipcode')
         logged_in_employee.name = name_from_form
         logged_in_employee.address = address_from_form
         logged_in_employee.zip_code = zip_from_form
         logged_in_employee.save()
-        return HttpResponseRedirect(reverse('employee:index'))
+        return HttpResponseRedirect(reverse('employees:index'))
     else:
         context = {
             'logged_in_employee': logged_in_employee
         }
-        return render(request, 'employee/edit_profile.html', context)
+        return render(request, 'employees/edit_profile.html', context)
